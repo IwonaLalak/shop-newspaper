738b8cc6835c6f73cd9d076d521889523b4892fd
Iwona Lalak <ilalak@topsa.com.pl>
2020-08-21 15:50:42 +0200

    layour wrapper for newspaper

--- src/views/NewspapersView/NewspapersView.tsx
+++ src/views/NewspapersView/NewspapersView.tsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import LayoutWrapper from "../../_sharedComponents/dedicated/layout/LayoutWrapper";
 
 class NewspapersView extends Component {
     render() {
         return (
-            <div>
-                NewspapersView list
-            </div>
+            <LayoutWrapper>
+                <h1>
+                    NewspapersView list
+                </h1>
+                <p>
+                    test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test
+                </p>
+                <p>
+                    test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test
+                </p>
+            </LayoutWrapper>
         );
     }
 }
